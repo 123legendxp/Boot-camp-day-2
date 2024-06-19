0fde2a7be60b2d51bc9666883ef8f1d030c668e5
123legendxp <Achilles.hofilena@aclcbutuan.edu.ph.com>
2024-06-19 12:37:29 +0800

    first commit

---
+++ Act1.py
@@ -0,0 +1,7 @@
+word = "summer bootcamp"
+print("#" + word.title())  
+title_cased = word.title()
+modified_string = "#" + title_cased[:-1] + title_cased[-1].upper()
+print(modified_string)  
+print("#" + word[7:10].title()) 
+print("#" + word[12].upper() + word[5])  
